ad915fda15836bd55672ce85b9a3e64f918e548e
Aaron Winter <Aaron Winter>
2016-01-10 12:33:07 +0000

    Removed win32 code from exceptions

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -5,26 +5,6 @@
 using namespace MOS_6502;
 using namespace std;
 
-#ifdef _WIN32
-//--UnknownOpCodeException
-const char* UnknownOpCodeException::what() const
-{
-    string error = "Unknown OpCode Given(base 10): " + to_string(_opcode);
-
-    return error.c_str();
-}
-
-UnknownOpCodeException::UnknownOpCodeException(unsigned int opcode)
-{
-    _opcode = opcode;
-}
-
-//--BitLimitExceededException
-const char* BitLimitExceededException::what() const
-{
-    return "Exceeded number of bits that this system is capable of storing! (8 bits)";
-}
-#else
 //--UnknownOpCodeException
 const char* UnknownOpCodeException::what() const noexcept
 {
@@ -43,4 +23,3 @@ const char* BitLimitExceededException::what() const noexcept
 {
     return "Exceeded number of bits that this system is capable of storing! (8 bits)";
 }
-#endif
--- Exceptions.h
+++ Exceptions.h
@@ -8,30 +8,6 @@
 
 #ifndef _EXCEPTIONS_H_
 #define _EXCEPTIONS_H_
-#ifdef _WIN32
-namespace MOS_6502
-{
-    class UnknownOpCodeException : public std::exception
-    {
-        //Variables
-        unsigned int _opcode;
-
-    public:
-        //--Overriden Methods:
-        const char* what() const override;
-
-        //--Constructor
-        UnknownOpCodeException(unsigned int opcode);
-    };
-
-    class BitLimitExceededException : public std::exception
-    {
-    public:
-        //--Overriden Methods:
-        const char* what() const override;
-    };
-}
-#else
 namespace MOS_6502
 {
     class UnknownOpCodeException : public std::exception
@@ -54,5 +30,4 @@ namespace MOS_6502
         const char* what() const noexcept override;
     };
 }
-#endif
 #endif
